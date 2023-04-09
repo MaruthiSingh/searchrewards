fb51ae07547a7adb0480921464204e4bffc24e2a
Maruthi Karthik Singh <singhkarthik261@gmail.com>
2023-04-09 12:08:15 +0530

    -v2

--- index.js
+++ index.js
@@ -10,20 +10,19 @@ app.use(express.static("public"));
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/main.html");
-    for (let i = 0; i < 1; i++) {
-        console.log(c.sentence());
-    }
+
 });
 
 app.post("/", function (req, res) {
 
-    for (let i in c.sentence()) {
+    for (let i = 0; i < 30; i++) {
         opn("https://www.bing.com/search?q=" + c.sentence())
             .then(() => {
                 console.log(c.sentence());
             })
             .catch((err) => {
                 console.error(err);
+                prompt("Error");
             });
     }
 });
