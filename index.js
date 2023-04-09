89edda3011709d4fd8a5050654477c90a822b20a
Maruthi Karthik Singh <singhkarthik261@gmail.com>
2023-04-09 12:43:54 +0530

    -v3 with ejs

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
 
-app.get("/", function (req, res) {
+app.get("searchrewards/", function (req, res) {
     res.render("main");
 });
 
